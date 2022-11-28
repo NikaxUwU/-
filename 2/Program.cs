2bf00e4063849e9c2099c69c0fdd85ef68c41492
Nika <krasnikova16071607@gmail.com>
2022-11-28 19:45:30 +0300

    1

--- 2/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿int num1 = Convert.ToInt32(Console.ReadLine());
-int num2 = Convert.ToInt32(Console.ReadLine());
-int num3 = Convert.ToInt32(Console.ReadLine());
-int max = 0;
-if (num1 > max) max = num1;
-if (num2 > max) max = num2;
-if (num3 > max) max = num3;
-Console.Write("Max = ");
-Console.WriteLine(max);
