74a3eae9da9a628db046697bea018e2e9030e345
Nika <krasnikova16071607@gmail.com>
2022-11-28 20:30:42 +0300

    Конец

---
+++ Homeworks/#3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 1
+
+// bool Test(int N)
+// {
+//     int num1 = N % 10;
+//     int num2 = N % 100 / 10;
+//     int num4 = N / 1000 % 10;
+//     int num5 = N / 10000;
+//     if(num1 == num5 && num2==num4)
+//     return true;
+//     return false;
+// }
+
+// Console.WriteLine("Введите число");
+// int N = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Test(N));
+
+
+// Задача 2
+
+// double GetLens(int x1, int y1, int z1, int x2, int y2, int z2)
+// {
+//     double len = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
+//     return len;
+// }
+// Console.WriteLine("Введите координаты первой точки");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты второй точки");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// int z2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(GetLens(x1,y1,z1,x2,y2,z2));
+
+// Задача 3
+
+// void Sqr3 (int N)
+// {
+//     Console.Write($"3 ->");
+//     for (int i = 1; i <= N; i++)
+//     {
+//         Console.Write($"  {i * i * i}");
+//     }
+// }
+
+// Console.WriteLine("Введите Число");
+// int N = Convert.ToInt32(Console.ReadLine());
+// Sqr3(N);
---
+++ Seminar/#3/1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*int FindQ (double x, double y)
+{
+    if (x>0 && y>0)
+    return 1;
+    if (x<0 && y>0)
+    return 2;
+    if (x<0 && y<0)
+    return 3;
+    if (x>0 && y<0)
+    return 4;
+
+    return 0;
+}
+Console.WriteLine("Введите координату X");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Y");
+double y = Convert.ToDouble(Console.ReadLine());
+int res = FindQ(x, y);
+Console.WriteLine($"Номер четверти {res}");;*/
+
+// АААААААААААААААААА 
+
+// double GetLens(int x1, int y1, int x2, int y2)
+// {
+//     double len = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+//     return len;
+// }
+
+// Console.WriteLine(GetLens(3,6,2,1));
+
+// void Sqr (int N)
+// {
+//     for (int i = 1; i <= N; i++)
+//     {
+//         Console.WriteLine($"{i} -> {i * i}");
+//     }
+// }
+
+// Console.WriteLine("Введите Число");
+// int N = Convert.ToInt32(Console.ReadLine());
