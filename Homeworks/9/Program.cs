5e952fbc79e4d30933d939e128cb29e4adee90c5
Nika <krasnikova16071607@gmail.com>
2022-12-08 21:28:20 +0300

    Конец

---
+++ Homeworks/9/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задач 2
+
+// int Summ(int n, int m)
+// {
+//     if (n > m)
+//     {
+//         return Summ(n-1, m) + n;
+//     }
+//     return n;
+// }
+
+// int m = Convert.ToInt32(Console.ReadLine());
+// int n = Convert.ToInt32(Console.ReadLine());
+// int x = Summ(n, m);
+// Console.WriteLine(x);
+
+//Задача 1
+
+// void num(int n)
+// {
+//     if (n >= 1)
+//     {
+//         Console.Write(n + " ");
+//         num(n-1);
+//     }
+// }
+// int n = Convert.ToInt32(Console.ReadLine());
+// num(n);
+
+
+// Задача 3
+
+int A(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    if (m == 0 && n != 0)
+    {
+        return A(n - 1, 1);
+    }
+    else 
+    {
+        return A(n - 1, A(n, m - 1));
+    } 
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+int x = A(n, m);
+Console.WriteLine(x);
